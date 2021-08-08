c729b7acd6506c947d214b9ade0da2e986bdf648
ErastusNjaramba <erastus.kimani@student.moringaschool.com>
2021-08-08 16:52:20 +0300

    add style to corm component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { Repo } from '../models/repo';
+import { UserService } from '../services/user.service';
+
 
 @Component({
   selector: 'app-form',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  Users!: User; 
+  Repos : Repo []=[]
+  constructor(public userService:UserService) { 
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
+  ngOnInit() {
+    this.searchGit("ErastusNjaramba")
+  }
+searchGit(searchTerm: string){
+  this.userService.searchGit(searchTerm).then(
+    (success)=>{
+      this.Users = this.userService.Users;
+    },
+    (error)=>{
+      console.log(error)
+      })
+    this.userService.searchRepository(searchTerm).then(
+      (success)=>{
+      this.Repos=this.userService.Repository
+
+    })
+  }
 }
